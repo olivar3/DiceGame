85571a6661485639cf5e47b20e643c63ecfb407c
olivar3 <olivar3@outlook.com>
2017-07-24 07:19:57 -0500

    Add files via upload

---
+++ dicegame/21.js
@@ -0,0 +1,18 @@
+function play(result){
+
+  var result = new Array()
+  var Dice = []
+  var Total = 0;
+  for (var i = 0; i < 6; i++)
+  {
+    Dice[i] = roll();
+    Total += Dice[i];
+  }
+}
+function roll(value){
+
+   var Roll = Math.floor((Math.random() * 6) + 1);{}
+   return value;
+   }
+ }
+roll()
